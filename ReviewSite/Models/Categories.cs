4a7b68b3ceb1876b60c7ac5798b1aed50b1a31c5
drobin45 <drobin45@kent.edu>
2017-07-28 10:49:10 -0400

    Added models, and controllers. Filled out several reviews and categories of reviews

---
+++ ReviewSite/Models/Categories.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ReviewSite.Models
+{
+    public class Categories
+    {
+        [Key]
+        public int CategoryID { get; set; }
+
+        [Display(Name ="Category")]
+        public string CategoryName { get; set; }
+
+        public virtual ICollection<Reviews> Reviews { get; set; }
+
+    }
+}
---
+++ ReviewSite/Models/Reviews.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ReviewSite.Models
+{
+    public class Reviews
+    {
+        [Key]
+        public int ReviewID { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        [Display(Name ="Publish Date")]
+        public DateTime PublishDate { get; set; }
+
+        [ForeignKey ("Categories")]
+        public int CategoryID { get; set; }
+        public virtual Categories Categories { get; set; }
+
+
+    }
+}
